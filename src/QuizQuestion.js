271ce03ebecef417217529ee154ceee9a5b4d92b
James Brett <jmsbrett@gmail.com>
2019-03-31 12:12:10 -0500

    changes

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-
-
+import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
   render() {
@@ -13,9 +12,10 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <li>
+            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+            {/* <li>
               {this.props.quiz_question.answer_options[0]}
-            </li>
+            </li> */}
           </ul>
         </section>
       </main>
